8d8e4ee0e869fffc3ace795014eb556052ad0268
maarjaryytel <maarja@ryytel.eu>
2019-10-27 10:02:49 +0200

    palindrome

--- kodune_ylesanne_palindrome/Program.cs
+++ kodune_ylesanne_palindrome/Program.cs
@@ -6,7 +6,27 @@ namespace kodune_ylesanne_palindrome
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string userChoice;
+
+			Console.WriteLine("Enter your word or name: ");
+			userChoice = Console.ReadLine();
+			Console.WriteLine("\n");
+
+			string reverseName;
+			char[] ch = userChoice.ToCharArray();
+			
+			Array.Reverse(ch);
+			reverseName = new string(ch);
+
+			if (reverseName.ToLower() == userChoice.ToLower())
+			{
+				Console.WriteLine("Palindrome!");
+			}
+			else
+			{
+				Console.WriteLine("It is not Palindrome!");
+			}
+			Console.ReadLine();
 		}
 	}
 }
